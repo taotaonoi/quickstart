1211578a428c591f552dd820bdd2ba66e587c4d5
taotanoi <itao.huhu@gmail.com>
2017-09-05 18:27:03 +0700

    Angular Service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,12 @@
 import { Component } from '@angular/core';
-
+import { HeroService } from './hero.service';
 
 export class Hero{
 	id: number;
 	name: string;
 }
 
-const HEROES: Hero[] = [
-{ id: 11, name: 'Mr. Nice' },
-{ id: 12, name: 'Narco' },
-{ id: 13, name: 'Bombasto' },
-{ id: 14, name: 'Celeritas' },
-{ id: 15, name: 'Magneta' },
-{ id: 16, name: 'RubberMan' },
-{ id: 17, name: 'Dynama' },
-{ id: 18, name: 'Dr IQ' },
-{ id: 19, name: 'Magma' },
-{ id: 20, name: 'Tornado' }
-];
+constructor(private heroService: HeroService){}
 
 @Component({
 	selector: 'my-app',
@@ -87,7 +76,7 @@ const HEROES: Hero[] = [
 
 export class AppComponent  { 
 	title = 'Tour of Heroes';
-	heroes = HEROES;
+	heroes = Hero[];
 	/*hero: Hero = {
 		id: 1,
 		name: 'Windstorm'
